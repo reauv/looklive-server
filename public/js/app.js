382b38a9dc0895c7faad76172a45fe11cb39adf6
Robert van Steen <studio@rovansteen.nl>
2016-03-11 10:12:13 +0100

    Optimize HTML/CSS

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
-    $('.product').first().addClass('product-active');
-    $(".product-indicator[data-uuid='"+$('.product').first().data('uuid')+"']").addClass('product-indicator-active');
+    $('.product').first().addClass('is-active');
+    $(".product-indicator[data-uuid='"+$('.product').first().data('uuid')+"']").addClass('is-active');
 
     $('.product-indicator').on('click', function(e){
         var id = $(e.currentTarget).data('uuid');
-        $('.product-indicator-active').removeClass('product-indicator-active');
-        $(e.currentTarget).addClass('product-indicator-active');
-        $('.product.product-active').removeClass('product-active');
-        $(".product[data-uuid='"+id+"']").addClass('product-active');
+        $('.product-indicator-active').removeClass('is-active');
+        $(e.currentTarget).addClass('is-active');
+        $('.product.product-active').removeClass('is-active');
+        $(".product[data-uuid='"+id+"']").addClass('is-active');
     });
 });
